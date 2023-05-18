ad7d5b3b536bbbf27ba9d43e8eaffd29f4d91363
Bruno Luis <bruno.dev07@gmail.com>
2023-05-18 11:02:59 -0300

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
+import { useEffect, useState } from 'react';
 import Container from './components/Container';
 
 const Item = ({ item }) => {
+
+  
+
+
   return (
     <li className='item' data-testid='product-item'>
       { /* TODO */ }
-      <img src="thumbnail here"></img>
-      <h4> title here </h4>
-      <p> description here </p>
-      <span> brand here </span>
-      <span> price here </span>
+      <img src={item.thumbnail} alt={item.title}></img>
+      <h4>{item.title}</h4>
+      <p>{item.description}</p>
+      <span>{item.brand}</span>
+      <span>{item.price}</span>
     </li>
   )
 }
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('products.json')
+      const data = await response.json()
+      setProducts(data)
+    }
+    fetchData()
+  }, [])
+
+  const smartphones = products.filter(product => product.category === 'smartphones')
+
   return (
     <Container>
       <ul className='list' data-testid='product-list'>
-        { /* TODO */ }
-        <Item />
+        {
+          smartphones.map(smartphone => (
+            <Item item={smartphone} />
+          ))
+        }
       </ul>
     </Container>
   );
