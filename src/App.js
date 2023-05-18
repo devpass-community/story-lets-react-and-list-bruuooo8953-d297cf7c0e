14950a1327a2313c349bb3eef16e05e6c53102a2
Bruno Luis <bruno.dev07@gmail.com>
2023-05-18 12:52:13 -0300

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
+import { useEffect, useState } from 'react';
 import Container from './components/Container';
+import products from './products.json'
 
 const Item = ({ item }) => {
   return (
     <li className='item' data-testid='product-item'>
-      { /* TODO */ }
-      <img src="thumbnail here"></img>
-      <h4> title here </h4>
-      <p> description here </p>
-      <span> brand here </span>
-      <span> price here </span>
+      <img src={item.thumbnail} alt={item.title} />
+      <h4>{item.title}</h4>
+      <p>{item.description}</p>
+      <span>{item.brand}</span>
+      <span>{item.price}</span>
     </li>
-  )
+  );
 }
 
 function App() {
+  const [productsList, setProductsList] = useState([])
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    setProductsList(products)
+    const filter = productsList.filter(product => product.category === 'smartphones')
+    setFilteredProducts(filter)
+  })
+
   return (
     <Container>
       <ul className='list' data-testid='product-list'>
-        { /* TODO */ }
-        <Item />
+        {filteredProducts.map(item => (
+          <Item key={item.id} item={item} />
+        ))}
       </ul>
     </Container>
   );
