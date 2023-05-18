8836bf99f64e8dd836fc3e50971f5836d00da8d1
Bruno Luis <bruno.dev07@gmail.com>
2023-05-18 11:54:28 -0300

    My fifth commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
+import { useEffect, useState } from 'react';
 import Container from './components/Container';
 
 const Item = ({ item }) => {
   return (
     <li className='item' data-testid='product-item'>
-      <img src={item.thumbnail} alt={item.title}></img>
+      <img src={item.thumbnail} alt={item.title} />
       <h4>{item.title}</h4>
       <p>{item.description}</p>
       <span>{item.brand}</span>
       <span>{item.price}</span>
     </li>
-  )
+  );
 }
 
 function App() {
-  
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
-  const response = fetch('src/products.json')
-  const data = response.json()
-  const filteredProducts = data.filter(product => product.category === 'smartphones')
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('src/products.json');
+      const data = await response.json();
+      const filteredData = data.filter(product => product.category === 'smartphones');
+      setFilteredProducts(filteredData);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <Container>
       <ul className='list' data-testid='product-list'>
-        {
-          filteredProducts.map(item => (
-            <Item item={item} />
-          ))
-        }
+        {filteredProducts.map(item => (
+          <Item key={item.id} item={item} />
+        ))}
       </ul>
     </Container>
   );
